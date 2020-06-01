35debfb97081f90415b60d411fafa2d0ad547f6e
klopfstein <dimitri.kft@gmail.com>
2020-06-01 22:30:43 +0200

    training form, token csrf

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Pin;
 use App\Repository\PinRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,7 +17,29 @@ class PinsController extends AbstractController
      */
     public function index(PinRepository $repo): Response
     {
-       dd($repo);
+        $pins = $repo->findAll();
         return $this->render('pins/index.html.twig',compact('pins'));
+    }
+
+      /**
+     * @Route("/pins/create", name="create", methods="GET|POST")
+     */
+    public function create(Request $request, EntityManagerInterface $em)
+    {
+       if($request->isMethod('POST'))
+       {
+           $data = $request->request->all();
+
+           $pin = new Pin;
+           $pin->setTitle($data['title']);
+           $pin->setDescription($data['description']);
+           $em->persist($pin);
+           $em->flush();
+
+           dd($data);
+
+           return $this->redirect('/');
+       }
+        return $this->render('pins/create.html.twig');
     }
 }
