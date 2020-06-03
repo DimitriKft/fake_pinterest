be46839ae6b9177f71386c256e5a6479e7796f61
klopfstein <dimitri.kft@gmail.com>
2020-06-03 09:49:51 +0200

    request form

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -8,12 +8,15 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PinsController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/", name="home", methods={"GET"})
      */
     public function index(PinRepository $repo): Response
     {
@@ -24,22 +27,31 @@ class PinsController extends AbstractController
       /**
      * @Route("/pins/create", name="create", methods="GET|POST")
      */
-    public function create(Request $request, EntityManagerInterface $em)
+    public function create(Request $request, EntityManagerInterface $em): Response
     {
-       if($request->isMethod('POST'))
-       {
-           $data = $request->request->all();
-           if($this->isCsrfTokenValid('pins_create', $data['_token']))
-           {
-               $pin = new Pin;
-               $pin->setTitle($data['title']);
-               $pin->setDescription($data['description']);
-               $em->persist($pin);
-               $em->flush();
-               
-            }
-               return $this->redirectToRoute('home');
-       }
-        return $this->render('pins/create.html.twig');
+        $form = $this->createFormBuilder()
+             ->add('title', TextType::class)
+             ->add('description', TextareaType::class)     
+             ->add('submit', SubmitType::class, ['label' => 'Create Pin'])
+             ->getForm()
+        ;
+      
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+           $data = $form->getData(); 
+
+           $pin = new Pin;
+           $pin->setTitle($data['title']);
+           $pin->setDescription($data['description']);
+           $em->persist($pin);
+           $em->flush(); 
+
+           return $this->redirectToRoute('home');
+        }
+
+        return $this->render('pins/create.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
